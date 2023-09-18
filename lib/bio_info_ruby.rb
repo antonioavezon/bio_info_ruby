f254da5722424693ac092b21184d328cb4eb8e8f
Antonio Avezon <antonio.avezon@gmail.com>
2023-09-18 16:11:33 -0300

    Primer commit: estructura inicial de la gema

---
+++ lib/bio_info_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bio_info_ruby/version"
+
+module BioInfoRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bio_info_ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BioInfoRuby do
+  it "has a version number" do
+    expect(BioInfoRuby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
